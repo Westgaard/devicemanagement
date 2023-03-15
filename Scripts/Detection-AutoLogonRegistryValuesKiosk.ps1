6daa304668529da5831b30395238a97887011117
Westgaard <45110532+Westgaard@users.noreply.github.com>
2023-03-15 10:13:08 +0100

    Update Detection-AutoLogonRegistryValuesKiosk.ps1

--- Scripts/Detection-AutoLogonRegistryValuesKiosk.ps1
+++ Scripts/Detection-AutoLogonRegistryValuesKiosk.ps1
@@ -3,12 +3,12 @@ $AutoLogonSID = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVe
 $DefaultUserName = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -ErrorAction SilentlyContinue).DefaultUsername
 $IsConnectedAutoLogon = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "IsConnectedAutoLogon" -ErrorAction SilentlyContinue).IsConnectedAutoLogon
 
-If (!($AutoAdminLogon -and $DefaultUserName -and $IsConnectedAutoLogon -and $AutoLogonSID)) {
+If (($AutoAdminLogon -ne 0) -or ($null -eq $AutoLogonSID) -or ($DefaultUserName -ne "KioskUser0") -or ($IsConnectedAutoLogon -ne 0)) {
     Write-Host "Registry-verdier satt feil! AutoAdminlogon: $autoadminlogon, DefaultUsername: $DefaultUserName, IsConnectedAutoLogon: $IsConnectedAutoLogon, AutoLogonSID: $AutoLogonSID"
     Exit 1
 }
 
 Else {
-    Write-Host "OK! AutoAdminlogon: $autoadminlogon, DefaultUsername: $DefaultUserName, IsConnectedAutoLogon: $IsConnectedAutoLogon, AutoLogonSID: $AutoLogonSID"
+    Write-Host "AutoAdminlogon: $autoadminlogon, DefaultUsername: $DefaultUserName, IsConnectedAutoLogon: $IsConnectedAutoLogon, AutoLogonSID: $AutoLogonSID"
     Exit 0
     }
