16e86ed1979b39d7e3c2dcf660b3c89719e5e2f0
Westgaard <45110532+Westgaard@users.noreply.github.com>
2023-03-15 10:13:23 +0100

    Update Remediation-AutoLogonRegistryValuesKiosk.ps1

--- Scripts/Remediation-AutoLogonRegistryValuesKiosk.ps1
+++ Scripts/Remediation-AutoLogonRegistryValuesKiosk.ps1
@@ -2,3 +2,10 @@ Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -Type String -Value "KioskUser0" -ErrorAction SilentlyContinue
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "IsConnectedAutoLogon" -Type Dword -Value 0 -ErrorAction SilentlyContinue
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoLogonSID" -Value (Get-LocalUser -Name kioskuser0 | select SID).SID.Value -Force
+
+$AutoAdminLogon = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoAdminLogon" -ErrorAction SilentlyContinue).AutoAdminLogon
+$AutoLogonSID = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AUtoLogonSID" -ErrorAction SilentlyContinue).AutoLogonSID
+$DefaultUserName = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -ErrorAction SilentlyContinue).DefaultUsername
+$IsConnectedAutoLogon = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "IsConnectedAutoLogon" -ErrorAction SilentlyContinue).IsConnectedAutoLogon
+
+Write-Host "Registry-verdier satt! AutoAdminlogon: $autoadminlogon, DefaultUsername: $DefaultUserName, IsConnectedAutoLogon: $IsConnectedAutoLogon, AutoLogonSID: $AutoLogonSID"
