99cbb5d2d94c2b9b29590df422b2ccf05845b99e
Westgaard <45110532+Westgaard@users.noreply.github.com>
2023-03-15 11:53:11 +0100

    Update Detection-AutoLogonRegistryValuesKiosk.ps1

--- Scripts/Detection-AutoLogonRegistryValuesKiosk.ps1
+++ Scripts/Detection-AutoLogonRegistryValuesKiosk.ps1
@@ -3,7 +3,7 @@ $AutoLogonSID = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVe
 $DefaultUserName = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -ErrorAction SilentlyContinue).DefaultUsername
 $IsConnectedAutoLogon = (Get-Itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "IsConnectedAutoLogon" -ErrorAction SilentlyContinue).IsConnectedAutoLogon
 
-If (($AutoAdminLogon -ne 0) -or ($null -eq $AutoLogonSID) -or ($DefaultUserName -ne "KioskUser0") -or ($IsConnectedAutoLogon -ne 0)) {
+If (($AutoAdminLogon -ne 1) -or ($null -eq $AutoLogonSID) -or ($DefaultUserName -ne "KioskUser0") -or ($IsConnectedAutoLogon -ne 0)) {
     Write-Host "Registry-verdier satt feil! AutoAdminlogon: $autoadminlogon, DefaultUsername: $DefaultUserName, IsConnectedAutoLogon: $IsConnectedAutoLogon, AutoLogonSID: $AutoLogonSID"
     Exit 1
 }
